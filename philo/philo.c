b6d228a0acd47e9d3ae80862a38d18f790d6ff21
Mohamed Azhari <mazhari@e1r6p7.1337.ma>
2022-03-28 21:21:39 +0000

    fixing some bugs

---
+++ philo/philo.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 20:37:47 by mazhari           #+#    #+#             */
+/*   Updated: 2022/03/28 21:00:13 by mazhari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	eating(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->eat);
+	philo->death_time = get_time() + philo->data->time_to_die;
+	pthread_mutex_unlock(&philo->eat);
+	print(*philo, "is eating");
+	philo->nbr_eat++;
+	if (philo->nbr_eat == philo->data->nbr_must_eat)
+		philo->data->finish_eat++;
+	usleep((philo->data->time_to_eat * 1000));
+}
+
+void	taken_a_fork(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo)->fork);
+	print(*philo, "has taken a fork");
+	if (philo->nbr == philo->data->nbr_of_philo - 1)
+	{
+		pthread_mutex_lock(&(philo - philo->nbr)->fork);
+		print(*philo, "has taken a fork");
+		eating(philo);
+		pthread_mutex_unlock(&(philo - philo->nbr)->fork);
+		pthread_mutex_unlock(&(philo)->fork);
+	}
+	else
+	{
+		pthread_mutex_lock(&(philo + 1)->fork);
+		print(*philo, "has taken a fork");
+		eating(philo);
+		pthread_mutex_unlock(&(philo + 1)->fork);
+		pthread_mutex_unlock(&(philo)->fork);
+	}
+}
+
+void	*death_fnc(void *p)
+{
+	t_philo	*philo;
+	int		time;
+
+	philo = p;
+	while (1)
+	{
+		if (get_time() >= philo->death_time)
+		{
+			pthread_mutex_lock(&philo->eat);
+			philo->data->death = 1;
+			print(*philo, "died");
+			return (NULL);
+		}
+	}
+}
+
+void	*philosopher(void *p)
+{
+	t_philo		*philo;
+	pthread_t	death;
+
+	philo = p;
+	philo->death_time = get_time() + philo->data->time_to_die;
+	pthread_create(&death, NULL, &death_fnc, philo);
+	pthread_detach(death);
+	while (1)
+	{
+		print(*philo, "is thinking");
+		taken_a_fork(philo);
+		if (philo->nbr_eat == philo->data->nbr_must_eat)
+			return (NULL);
+		print(*philo, "is sleeping");
+		usleep((philo->data->time_to_sleep * 1000));
+	}
+	return (NULL);
+}
--- philo/philo.h
+++ philo/philo.h
@@ -6,25 +6,25 @@
 /*   By: mazhari <mazhari@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 14:26:34 by mazhari           #+#    #+#             */
-/*   Updated: 2022/03/18 19:41:24 by mazhari          ###   ########.fr       */
+/*   Updated: 2022/03/28 20:54:57 by mazhari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/time.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/time.h>
 
 typedef struct s_philo
 {
 	pthread_t		philo;
-	pthread_mutex_t fork;
-	pthread_mutex_t eat;
+	pthread_mutex_t	fork;
+	pthread_mutex_t	eat;
 	int				nbr_eat;
 	int				nbr;
 	long			death_time;
@@ -33,23 +33,23 @@ typedef struct s_philo
 
 typedef struct s_data
 {
-	t_philo *p;
-	int nbr_of_philo;
-	int time_to_die;
-	int	time_to_eat;
-	int time_to_sleep;
-	int	nbr_must_eat;
-	long start_time;
-	int	death;
-	int finish_eat;
-    pthread_mutex_t print;
+	t_philo			*p;
+	int				nbr_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nbr_must_eat;
+	long			start_time;
+	int				death;
+	int				finish_eat;
+	pthread_mutex_t	print;
 }				t_data;
 
 int		ft_atoi(const char *str);
 int		data_init(t_data *data, char **av);
 void	*philosopher(void *p);
-int 	exit_program(t_philo  *philo);
-long	get_time();
-void	print(t_philo  philo,  char *str);
+int		exit_program(t_data *data);
+long	get_time(void);
+void	print(t_philo philo, char *str);
 
 #endif 
